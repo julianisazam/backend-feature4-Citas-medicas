44c69035e15a6fe2ad0dcdd2ca2dfca9a3832888
Miguel Angel Serna Montoya <110539006+MiguelSernaMon@users.noreply.github.com>
2025-06-10 17:12:46 -0500

    disabled CORS

---
+++ maintenance-alerts-service/src/main/java/com/feature4/maintenance_alerts_service/config/CorsConfig.java
@@ -0,0 +1,42 @@
+package com.feature4.maintenance_alerts_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Permite peticiones desde cualquier origen
+        config.addAllowedOrigin("*");
+        // Permite todos los encabezados
+        config.addAllowedHeader("*");
+        // Permite todos los métodos HTTP
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
